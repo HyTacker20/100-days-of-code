9945a8da1ba35dbf0cc4531ced930ea4a9215605
Andrii Hermak <hermak20102004@gmail.com>
2023-11-16 19:45:10 +0100

    Day 6

---
+++ Day 6 - Escaping the Maze/main.py
@@ -0,0 +1,37 @@
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+
+def in_field():
+    if front_is_clear() and right_is_clear():
+        turn_left()
+        turn_left()
+        if front_is_clear() and right_is_clear():
+            turn_left()
+            turn_left()
+            return True
+        turn_left()
+        turn_left()
+    return False
+
+
+while not at_goal():
+    if in_field():
+        move()
+        if front_is_clear():
+            move()
+    while front_is_clear() and wall_on_right():
+        move()
+    if right_is_clear() and wall_in_front():
+        turn_right()
+    elif front_is_clear() and not right_is_clear():
+        move()
+    else:
+        turn_left()
+    if front_is_clear():
+        move()
+done()
